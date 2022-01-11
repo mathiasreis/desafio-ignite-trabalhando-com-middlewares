8b4f51b568fd05570860812e2a5ee82ed76ec9e3
Alexsander Reis <alexsander.arsilva@gmail.com>
2022-01-11 10:54:13 -0400

    create checksExistsUserAccount and findUserById middlewares

--- src/index.js
+++ src/index.js
@@ -10,7 +10,17 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user) {
+    return response.status(404).json({ error: "Username not found!"});
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
@@ -22,7 +32,17 @@ function checksTodoExists(request, response, next) {
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user  = users.find(user => user.id === id);
+
+  if(!user) {
+    return response.status(404).json({ error: "User not found with the id provided"});
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
